789b77a57a7c15026d1533178db5260927f1c3fc
raffysz <raffysz@outlook.com>
2016-02-24 14:51:14 +0000

    Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+        <body>
+            <p>
+                <?php
+                echo "Heelo World";
+                ?>
+            </p>
+        </body>
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 0907562
+ * Date: 24/02/2016
+ * Time: 14:49
+ */
