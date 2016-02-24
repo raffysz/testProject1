a3963495363fac22d91f4f06e31cf71587e8ba40
raffysz <raffysz@outlook.com>
2016-02-24 14:57:35 +0000

    Commit

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
         <body>
             <p>
                 <?php
-                echo "Hello World";
+                echo "Hello, World!";
+                echo "Hello,"." "."World"."!";
                 ?>
             </p>
         </body>
