c06c02399054189b1720530be42827d866fdfd39
raffysz <raffysz@outlook.com>
2016-03-02 15:41:44 +0000

    Commit

---
+++ week5/connection.php
@@ -0,0 +1,13 @@
+<?php
+define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
+define('DB_USERNAME', 'bff8632343690c');
+define('DB_PASSWORD', '8d620ab9');
+define('DB_DATABASE', 'raffaele0907562');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
+/**
+ * Created by PhpStorm.
+ * User: 0907562
+ * Date: 02/03/2016
+ * Time: 15:20
+ */
---
+++ week5/home.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+
+<body>
+<h1>Hello</h1>
+</body>
+</html>
+
+/**
+ * Created by PhpStorm.
+ * User: 0907562
+ * Date: 02/03/2016
+ * Time: 15:41
+ */
