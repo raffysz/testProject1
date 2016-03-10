b09ccc35e1fd0d0d47e0e460053995b324279043
raffy <raffysz@outlook.com>
2016-03-10 11:56:06 +0000

    commit

--- index.php
+++ index.php
@@ -3,9 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <title>Bug & Job Tracking System</title>
+
     <link rel="stylesheet" href="style.css" type="text/css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
 </head>
+
 <body>
+
 <!--START OF HEADER -->
 <header>
     <div id="title">
@@ -32,5 +38,7 @@
 
 </footer>
 <!--END OF FOOTER -->
+
 </body>
+
 </html>
