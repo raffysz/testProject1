939c35d18a858905e1e31652d62f7eccb38c5a04
raffy <raffysz@outlook.com>
2016-03-15 19:18:10 +0000

    Commit

---
+++ login.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+
+    <title>Bug & Job Tracking System</title>
+
+    <link rel="stylesheet" href="style.css" type="text/css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+</head>
+
+<body>
+
+<!--START OF HEADER -->
+<header class="header">
+
+    <h1 style="vertical-align:middle">Job Traking System</h1>
+    <img style="vertical-align:middle" src="images/Robert-Gordon-University-logo.jpg" alt="Bug & Job Logo"/>
+
+</header>
+<!--END OF HEADER -->
+
+<!--START OF MAIN -->
+<main>
+
+</main>
+<!--END OF MAIN -->
+
+<!--START OF FOOTER -->
+<footer class="footer" style="text-align: center">
+
+    <h2 style="vertical-align:middle">Connect with Us</h2>
+
+    <a  href="http://www.rgu.ac.uk">
+        <img style="vertical-align:middle" src="images/rgulogo.jpg" alt="RGU Logo" ></a>
+    <a href="https://www.facebook.com/robertgordonuniversity">
+        <img style="vertical-align:middle" src="images/facebooklogo.jpg" alt="Facebook Logo" ></a>
+    <a href="https://twitter.com/RobertGordonUni?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">
+        <img style="vertical-align:middle" src="images/twitterlogo.jpg" alt="Twitter Logo" ></a>
+
+    <nav>
+
+        <p>
+            <a href ="index.html">Home</a> |
+            <a href ="#">About</a> |
+            <a href ="#">Useful Links</a> |
+            <a href ="#">Copyright ©2016</a>
+        </p>
+    </nav>
+
+</footer>
+<!--END OF FOOTER -->
+
+</body>
+</html>
