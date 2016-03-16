d89a16e7e19239f0dedb783e7929447f05678584
raffy <raffysz@outlook.com>
2016-03-16 12:03:19 +0000

    Commit

--- db_connect/loginscript.php
+++ db_connect/loginscript.php
@@ -1,7 +1,27 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: raffy
- * Date: 16/03/2016
- * Time: 10:20
- */
+
+function load($page='../pages/login.php')
+{
+    $url= 'http://'.$_SERVER['HTTP_HOST'].
+        dirname($_SERVER['PHP_SELF']);
+
+    $url = rtrim($url, '/\\');
+    $url = '/'. $page;
+
+    header ("location:$url");
+    exit();
+}
+
+function validate($db, $un =", $passwd=")
+{
+    $errors = array();
+
+    if (empty($un))
+    {
+        $errors[]='Enter your username.';
+    }
+    else
+    {
+        $e=mysqli_real_escape_string($db, trim($un));
+    }
+}
