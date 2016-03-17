8488d2213c66dc6dfcfc9d36c24eb7f379625f01
raffy <raffysz@outlook.com>
2016-03-17 19:44:43 +0000

    Commit

--- db_connect/post_action.php
+++ db_connect/post_action.php
@@ -20,6 +20,12 @@ if ($_SERVER['REQUEST_METHOD']=='POST')
         }
     }
 
+    if (empty($errors))
+    {$q = "SELECT title FROM bugs WHERE title='{$_POST['title']}'";
+        $r = mysqli_query($db, $q);
+        if (mysqli_num_rows($r)!=0)
+        {$errors[] = 'Title already exist, please use another.';}
+
     if (!empty($_POST['title'])&& !empty($_POST['description']))
     {
         require('../db_connect/connection.php');
