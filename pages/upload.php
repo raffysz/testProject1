42f97a787181d05d450dbf27d65dc8d325897aaf
raffy <raffysz@outlook.com>
2016-03-22 20:06:58 +0000

    commit

--- pages/upload.php
+++ pages/upload.php
@@ -57,7 +57,7 @@
     Result is "uploads/filename.extension" */
     $target_path = $target_path . basename( $_FILES['uploadedfile']['name']);
 
-    $target_path = "uploads/";
+    $target_path = "../pages/uploads/";
 
     $target_path = $target_path . basename( $_FILES['uploadedfile']['name']);
 
@@ -70,7 +70,7 @@
 
     ?>
 
-    <form enctype="multipart/form-data" action="uploader.php" method="POST">
+    <form enctype="multipart/form-data" action="../pages/upload.php" method="POST">
         <input type="hidden" name="MAX_FILE_SIZE" value="100000" />
         Choose a file to upload: <input name="uploadedfile" type="file" /><br />
         <input type="submit" value="Upload File" />
