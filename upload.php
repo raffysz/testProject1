ed72cca2e06d98083261a15c628a0f1ce3077466
raffy <raffysz@outlook.com>
2016-03-22 21:23:53 +0000

    commit

--- upload.php
+++ upload.php
@@ -1,24 +1,24 @@
 <?php
-if(isset($_FILES['image'])){
+if(isset($_FILES['upload'])){
     $errors= array();
-    $file_name = $_FILES['image']['name'];
-    $file_size =$_FILES['image']['size'];
-    $file_tmp =$_FILES['image']['tmp_name'];
-    $file_type=$_FILES['image']['type'];
-    $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+    $file_name = $_FILES['upload']['name'];
+    $file_size =$_FILES['upload']['size'];
+    $file_tmp =$_FILES['upload']['tmp_name'];
+    $file_type=$_FILES['upload']['type'];
+    $file_ext=strtolower(end(explode('.',$_FILES['upload']['name'])));
 
-    $expensions= array("jpeg","jpg","png");
+    $expensions= array("txt");
 
     if(in_array($file_ext,$expensions)=== false){
-        $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+        $errors[]="extension not allowed, please choose a TXT file.";
     }
 
     if($file_size > 2097152){
-        $errors[]='File size must be excately 2 MB';
+        $errors[]='File size must not exceed 2 MB';
     }
 
     if(empty($errors)==true){
-        move_uploaded_file($file_tmp,"images/".$file_name);
+        move_uploaded_file($file_tmp,"uploads/".$file_name);
         echo "Success";
     }else{
         print_r($errors);
@@ -29,7 +29,7 @@ if(isset($_FILES['image'])){
 <body>
 
 <form action="" method="POST" enctype="multipart/form-data">
-    <input type="file" name="image" />
+    <input type="file" name="upload" />
     <input type="submit"/>
 </form>
 
