993390c891e6d220f29a7312cb65f9750754e958
raffy <raffysz@outlook.com>
2016-03-22 20:29:48 +0000

    commit

--- pages/upload.php
+++ pages/upload.php
@@ -47,9 +47,47 @@
 				<dl class="btn"><a href="../pages/logout.php" title="Logout">Logout</a></dl>
 			</ul></nav>	
 		</div>';
+
+    if(isset($_POST['upload']) && $_FILES['userfile']['size'] > 0)
+    {
+        $fileName = $_FILES['userfile']['name'];
+        $tmpName  = $_FILES['userfile']['tmp_name'];
+        $fileSize = $_FILES['userfile']['size'];
+        $fileType = $_FILES['userfile']['type'];
+
+        $fp      = fopen($tmpName, 'r');
+        $content = fread($fp, filesize($tmpName));
+        $content = addslashes($content);
+        fclose($fp);
+
+        if(!get_magic_quotes_gpc())
+        {
+            $fileName = addslashes($fileName);
+        }
+        include '../db_connect/connection.php';
+        
+
+        $query = "INSERT INTO upload (name, size, type, content ) ".
+            "VALUES ('$fileName', '$fileSize', '$fileType', '$content')";
+
+        mysql_query($query) or die('Error, query failed');
+        include 'library/closedb.php';
+
+        echo "<br>File $fileName uploaded<br>";
+    }
 ?>
 
-    <form enctype="multipart/form-data" action="../db_connect/uploader.php" method="POST"> Please choose a file: <input name="uploaded" type="file" /><br /> <input type="submit" value="Upload" /> </form>
+    <form method="post" enctype="multipart/form-data">
+        <table width="350" border="0" cellpadding="1" cellspacing="1" class="box">
+            <tr>
+                <td width="246">
+                    <input type="hidden" name="MAX_FILE_SIZE" value="2000000">
+                    <input name="userfile" type="file" id="userfile">
+                </td>
+                <td width="80"><input name="upload" type="submit" class="box" id="upload" value=" Upload "></td>
+            </tr>
+        </table>
+    </form>
 
 </main>
 <!--END OF MAIN -->
