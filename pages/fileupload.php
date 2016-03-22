b04ca1c29017d3106260a83678bf5882f0936365
raffy <raffysz@outlook.com>
2016-03-22 21:42:10 +0000

    commit

--- pages/fileupload.php
+++ pages/fileupload.php
@@ -27,89 +27,42 @@
 <main>
 
     <?php
+    if(isset($_FILES['upload'])){
+        $errors= array();
+        $file_name = $_FILES['upload']['name'];
+        $file_size =$_FILES['upload']['size'];
+        $file_tmp =$_FILES['upload']['tmp_name'];
+        $file_type=$_FILES['upload']['type'];
+        $file_ext=strtolower(end(explode('.',$_FILES['upload']['name'])));
 
-    session_start();
+        $expensions= array("txt");
 
-    if (!isset($_SESSION['username']))
-    {
-        require ('../db_connect/login_tools.php');
-        load();
-    }
-
-    echo '<div id="sidebar">
-			<nav id="navigation"><ul>
-				<dl class="btn"><a href="../pages/loggedin.php" title="Home">Home</a></dl>
-				<dl class="btn"><a href="../pages/submitbug.php" title="New Bug">Submit new bug</a></dl>
-				<dl class="btn"><a href="../pages/listbugs.php" title="List All">List all bugs</a></dl>
-				<dl class="btn"><a href="../pages/retrievebug.php" title="Retrieve">Retrieve all bug info</a></dl>
-				<dl class="btn"><a href="../pages/comments.php" title="Post">Post a comment</a></dl>
-				<dl class="btn"><a href="../pages/fileupload.php" title="Upload">Upload a file</a></dl>
-				<dl class="btn"><a href="../pages/logout.php" title="Logout">Logout</a></dl>
-			</ul></nav>	
-		</div>';
-
-    $page_title = 'Registration Form';
-
-    if ($_SERVER['REQUEST_METHOD']=='POST')
-    {
-        require('../db_connect/connection.php');
-        $errors = array();
-
-        if (empty ($_POST['bugid']))
-        {$errors[] = 'Enter a bug ID.';}
-        else
-        {$bugid = mysqli_real_escape_string($db,
-            trim($_POST['bugid']));}
-
-        if (empty($errors))
-        {$q = "SELECT bugID FROM bugs WHERE bugID='$bugid'";
-            $r = mysqli_query($db, $q);
-            if (mysqli_num_rows($r)!=1)
-            {$errors[] = 'Invalid bug ID, to leave a comment please input a valid bug ID from: <a href="../pages/listbugs.php">Bugs List</a>.';}
+        if(in_array($file_ext,$expensions)=== false){
+            $errors[]="extension not allowed, please choose a TXT file.";
         }
 
-        if (empty($errors) && !empty($_POST['comment']))
-        {
-            $q = "INSERT INTO comments (description, postDate, userID, bugID) VALUES ('{$_POST['comment']}',NOW(),'{$_SESSION['userID']}','$bugid')";
-            $r = mysqli_query ($db, $q);
-            if ($r)
-            {echo '<h1>Form submitted successfully!</h1>
-        <p>Your comment is been submitted successfully and can now be view by selecting the bug ID here:</p>
-        <p><a href="../pages/retrievebug.php">Retrieve a Specific Bug</a></p>';
-            }
-            mysqli_close($db);
-            exit();
+        if($file_size > 2097152){
+            $errors[]='File size must not exceed 2 MB';
         }
-        else
-        {
-            echo '<h1 id="errmsg">Error!</h1>
-                <p id="errmsg">The following error(S) occurred:<br>';
-            foreach ($errors as $msg)
-            {
-                echo " - $msg<br>";
-            }
-            echo 'Please try again.</p>';
-            mysqli_close ($db);
+
+        if(empty($errors)==true){
+            move_uploaded_file($file_tmp,"../uploads/".$file_name);
+            echo "Success";
+        }else{
+            print_r($errors);
         }
     }
-
     ?>
+    
+    
 
-    <h1>Comment form</h1>
-    <p>Please check the correct bug ID before leaving a comment here:</p>
-    <p><a href="../pages/listbugs.php">View List Of Reported Bugs</a></p>
-
-    <form action="comments.php" method="POST">
-        <p>
-            Bug ID: <input type="text" name="bugid" required="required"
-                           value="<?php if (isset($_POST['bugid']))
-                               echo $_POST['bugid'];?>">
-        </p> <p>
-            Comment:
-            <br><textarea name="comment" required="required" rows="15" cols="50"></textarea></p>
-        <p><input type="submit" value="Submit"></p>
+    <form action="" method="POST" enctype="multipart/form-data">
+        <input type="file" name="upload" />
+        <input type="submit"/>
     </form>
 
+    
+    
 </main>
 <!--END OF MAIN -->
 
