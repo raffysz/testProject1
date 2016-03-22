add4dc62a5837b7855f9a265b01cefde8769bb87
raffy <raffysz@outlook.com>
2016-03-22 20:12:06 +0000

    commit

--- pages/upload.php
+++ pages/upload.php
@@ -70,7 +70,7 @@
 
     ?>
 
-    <form enctype="multipart/form-data" action="../pages/upload.php" method="POST">
+    <form enctype="multipart/form-data" action="../db_connect/uploader.php" method="POST">
         <input type="hidden" name="MAX_FILE_SIZE" value="100000" />
         Choose a file to upload: <input name="uploadedfile" type="file" /><br />
         <input type="submit" value="Upload File" />
